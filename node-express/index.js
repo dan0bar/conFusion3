46f735ab8e876253189b86d47680315f5dbb9f11
Daniil Barkov <barkovdc@gmail.com>
2022-06-14 14:23:45 +0200

    Added express Router

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const dishRouter = require("./routes/dishRouter");
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
+console.log('Using logging Middleware Morgan')
+app.use(morgan('dev'));
+
+console.log('Using static files')
+app.use(express.static(__dirname + '/public'))
+
+
+
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server!</h1></body></html>')
