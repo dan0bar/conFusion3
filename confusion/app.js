cd64ca059c33f195ca8a7778163e46e2ab6c0acd
Daniil Barkov <barkovdc@gmail.com>
2022-06-22 21:40:34 +0200

    Assignment 4

--- confusion/app.js
+++ confusion/app.js
@@ -1,60 +1,71 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter')
-var promoRouter = require('./routes/promoRouter')
-var leaderRouter = require('./routes/leaderRouter')
-var uploadRouter = require('./routes/uploadRouter')
+var index = require('./routes/index');
+var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
+var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favouriteRouter');
 
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
+
 const url = config.mongoUrl;
-const connect = mongoose.connect(url);
+
+const connect = mongoose.connect(url, {
+    useMongoClient: true
+});
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
+    console.log('Connected to server!');
 }, (err) => {
     console.log(err);
 });
 
 var app = express();
 
-// Secure traffic only
-app.all('*', (req, res, next) => {
+app.all('*', (req, res, nxt) => {
     if (req.secure) {
-        return next();
-    }
-    else {
+        return nxt();
+    } else {
         res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
     }
 });
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(session({
+    name: 'session-id',
+    secret: 'Jesus-Loves-w@ch!ra-so-Much!!',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
 app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -62,10 +73,13 @@ app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
